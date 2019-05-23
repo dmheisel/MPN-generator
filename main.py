3c099e48ca1a1caa22c86a569a4ce47874b10e43
David Heisel <d.m.heisel@gmail.com>
2019-05-22 20:19:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 import csv
+from tkinter import *
+
 
 
 #finds MPN value based on input, prints "value not found" if not on table
 
-ui = input("Input combination of positives:")
-with open('MPN-table.csv') as f:
-  for row in csv.reader(f):
-    if row[0] == ui:
-      print('MPN value is:' + row[1])
-      break
-  else:
-    print('MPN value not found')  
+#ui = input("Input combination of positives:")
+#with open('MPN-table.csv') as f:
+#  for row in csv.reader(f):
+#    if row[0] == ui:
+#      print('MPN value is:' + row[1])
+#      break
+#  else:
+#    print('MPN value not found')  
+
+
+class Window(Frame):
+  def __init__(self, master=None):
+    Frame.__init__(self, master)
+    self.master = master
+    self.init_window()
+
+  def init_window(self):
+    self.master.title("MPN Generator")
+    self.pack(fill = BOTH, expand = 1)
+      
+    menu = Menu(self.master)
+    self.master.config(menu = menu)
+    
+    file = Menu(menu)
+    file.add_command(label = 'Exit', command = self.client_exit)
+    menu.add_cascade(label = 'File', menu = file)
+    
+    edit = Menu(menu)
+    edit.add_command(label ='Undo')
+    menu.add_cascade(label = 'Edit', menu = edit)
+
+
+
+
+  def client_exit(self):
+    exit()
+
+
+root = Tk()
+root.geometry("400x300")
+app = Window(root)
+
+root.mainloop()
